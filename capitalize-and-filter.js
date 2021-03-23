02860273d500a24bf1a02d3945144c1a0436f325
zhaned <zhanedmond@gmail.com>
2021-03-22 18:08:23 -0700

    capitalize and filter with array methods

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,15 +1,16 @@
 const capitalizeAndFilter = (strings) => {
-  // const uppercase = strings.map(string => string.toUpperCase());
-  const uppercase = strings;
-  for(let i = 0; i < strings.length; i++) {
-    uppercase[i] = uppercase[i].toUpperCase();
-  }
-  for(let i = 0; i < strings.length; i++) {
-    if(uppercase[i].charAt(0) === 'F') {
-      uppercase.splice(i, i + 1);
-    }
-  }
-  return uppercase;
+  const uppercase = strings.map(string => string.toUpperCase());
+  const filtered = uppercase.filter(string => string[0] !== 'F');
+  // const uppercase = strings;
+  // for(let i = 0; i < strings.length; i++) {
+  //   uppercase[i] = uppercase[i].toUpperCase();
+  // }
+  // for(let i = 0; i < strings.length; i++) {
+  //   if(uppercase[i].charAt(0) === 'F') {
+  //     uppercase.splice(i, i + 1);
+  //   }
+  // }
+  return filtered;
 };
 
 module.exports = capitalizeAndFilter;
