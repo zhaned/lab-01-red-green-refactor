ca7f8643253609f9d8ea8a292132e08e64378cd6
zhaned <zhanedmond@gmail.com>
2021-03-22 17:49:54 -0700

    capitalize and filter F with for loops

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,6 +1,14 @@
 const capitalizeAndFilter = (strings) => {
-  const uppercase = strings.map(string => string.toUpperCase());
-
+  // const uppercase = strings.map(string => string.toUpperCase());
+  const uppercase = strings;
+  for(let i = 0; i < strings.length; i++) {
+    uppercase[i] = uppercase[i].toUpperCase();
+  }
+  for(let i = 0; i < strings.length; i++) {
+    if(uppercase[i].charAt(0) === 'F') {
+      uppercase.splice(i, i + 1);
+    }
+  }
   return uppercase;
 };
 
